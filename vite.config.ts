194b4da25b57e3a92584efc5bb78c61ff1244cd5
Christian D'Aquino <christian.daquino@gmail.com>
2024-12-27 21:47:13 +0800

    d

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VueEloquent'
+      name: 'VueEloquentV2'
     },
     sourcemap: true,
     rollupOptions: {
